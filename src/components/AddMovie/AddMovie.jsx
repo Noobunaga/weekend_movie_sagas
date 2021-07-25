7ac54defb059da3856aadf94257f0665bb83df02
Pouapheng Yang <phewy06@yahoo.com>
2021-07-25 15:03:13 -0500

    setting up AddMovie

---
+++ src/components/AddMovie/AddMovie.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
+import './AddMovie.css'
+
+function AddMovie() {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const genres = useSelector(store => store.genres);
+    const [title, setTitle] = useState('');
+    const [url, setUrl] = useState('');
+    const [description, setDescription] = useState('');
+    const [genre, setGenre] = useState('');
+
+    const newMovieData = () => {
+        event.preventDefault();
+        const newMovie = {title: title, poster: url, description: description, genre_id: genre}
+        console.log('Getting info from database', newMovie);
+        dispatch({type: 'POST_MOVIE', payload: newMovie})
+        setDescription('')
+        setTitle('')
+        setUrl('')
+    }
+    
+    const goToList = () => {
+        history.push('/')
+    }
+
+return(
+
+)
+}
